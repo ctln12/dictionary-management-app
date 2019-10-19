3ead9dbcab8a8a5e3e5996b9f1e0101c7c27a032
Cataline Baert <snuf_kin@yahoo.com>
2019-10-20 00:07:44 +0200

    create working app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,204 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      newDictionaryName: '',
+      newDomain: '',
+      newRange: '',
+      dictionaries: [],
+    }
+  }
+
+  componentDidMount = () => {
+    const data = [
+      {
+        id: 1,
+        name: 'Cactus',
+        content: [
+          {
+            id: 1,
+            domain: 'Opuntia',
+            range: 'round'
+          },
+          {
+            id: 2,
+            domain: 'Column',
+            range: 'long'
+          }
+        ]
+      },
+      {
+        id: 2,
+        name: 'Flowers',
+        content: [
+          {
+            id: 1,
+            domain: 'Orchid',
+            range: 'white'
+          }
+        ]
+      }
+    ];
+  }
+
+  updateInput(key, value){
+    // update react state (if use of localStorage)
+    this.setState({
+      [key]: value
+    })
+  }
+
+  addDictionary(){
+    // copy of current list of dictionaries
+    const dictionaries = [...this.state.dictionaries];
+
+    // create item with unique id
+    const newDictionaryName = {
+      id: 1 + dictionaries.length,
+      name: this.state.newDictionaryName.slice(),
+      content: []
+      // content: [{ id: 1 + Math.random(), domain: this.state.newDomain.slice(), range: this.state.newRange.slice()}]
+    };
+
+    //add new item to dictionaries
+    dictionaries.push(newDictionaryName);
+
+    //update state with new dictionaries and reset newDictionaryName input
+    this.setState({
+      dictionaries,
+      newDictionaryName: ''
+    });
+  }
+
+  deleteDictionary(id){
+    // copy current list of dictionaries
+    const dictionaries = [...this.state.dictionaries];
+
+    // filter out item being deleted
+    const updatedDictionaries = dictionaries.filter(dictionary => dictionary.id !== id);
+
+    this.setState({dictionaries: updatedDictionaries});
+  }
+
+  addNewRow(dictionary) {
+    // copy of current list of dictionaries
+    const content = dictionary.content;
+
+    // create item with unique id
+    const newContent = {
+      id: 1 + content.length,
+      domain: this.state.newDomain.slice(),
+      range: this.state.newRange.slice()
+    };
+
+    //add new item to dictionaries
+    dictionary.content.push(newContent);
+
+    //update state with new dictionaries and reset newDictionaryName input
+    this.setState({
+      dictionaries: this.state.dictionaries,
+      // content,
+      newDomain: '',
+      newRange: ''
+    });
+  }
+
+  deleteRow(dictionary, rowId) {
+    // copy current list of dictionaries
+    const content = dictionary.content;
+    console.log(content);
+    // filter out item being deleted
+    const updatedContent = content.filter(row => row.id !== rowId);
+    console.log(updatedContent);
+    // console.log(dictionary);
+    dictionary.content = updatedContent;
+
+    this.setState({ dictionaries: this.state.dictionaries});
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div>
+          <h1>App</h1>
+          <p>Create a new dictionary:</p>
+          <input
+          type='text'
+          placeholder='Name'
+          value={this.state.newDictionaryName}
+          onChange={e => this.updateInput('newDictionaryName', e.target.value)}
+          />
+          <button
+          onClick={() => this.addDictionary()}
+          >
+            add
+          </button>
+          <hr/>
+          <div className='Dictionaries'>
+            <h2>Dictionaries</h2>
+            {this.state.dictionaries.map(dictionary => {
+              return (
+                <div key={dictionary.id} className='Dictionary'>
+                  <h3 key={dictionary.id}>
+                    {dictionary.name}
+                    <button
+                      onClick={() => this.deleteDictionary(dictionary.id)}
+                    >
+                      x
+                    </button>
+                  </h3>
+                  <div className='NewEntry'>
+                    <p>Add new entry:</p>
+                    <input
+                      type='text'
+                      placeholder='domain'
+                      value={this.state.newDomain}
+                      onChange={e => this.updateInput('newDomain', e.target.value)}
+                    />
+                    <input
+                      type='text'
+                      placeholder='range'
+                      value={this.state.newRange}
+                      onChange={e => this.updateInput('newRange', e.target.value)}
+                    />
+                    <button onClick={() => this.addNewRow(dictionary)}>add new content</button>
+                  </div>
+                  <table>
+                    <thead>
+                      <tr>
+                        <th>Domain</th>
+                        <th>Range</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      {dictionary.content.map(row => {
+                        return (
+                          <tr key={row.id}>
+                            <td>{row.domain}</td>
+                            <td>{row.range}</td>
+                            <td>
+                              <button
+                                onClick={() => this.deleteRow(dictionary, row.id)}
+                              >
+                                x
+                              </button>
+                            </td>
+                          </tr>
+                        )
+                      })}
+                    </tbody>
+                  </table>
+                </div>
+              )
+            })}
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
